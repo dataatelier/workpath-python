617ae30499728f4c907c8fb22ee688a8163a78d1
Daniel Hafner <daniel-hafner1@gmx.de>
2020-05-02 23:43:39 +0200

    basic structure closes #1

---
+++ README.md
@@ -0,0 +1,8 @@
+# workpath
+
+This package allows you to interact with the [Workpath](https://www.workpath.com/) [API](https://developer.workpath.com/).
+
+## Installation
+```
+pip install workpath
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="workpath",
+    version="0.0.1",
+    author="Daniel Hafner",  # add yourself here setuptools
+    author_email="daniel-hafner1@gmx.de",
+    description="Package to interact with workpath API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dataatelier/workpath-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8.3",  # more will be added
+        "License :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.8.3"  # more to come
+)
