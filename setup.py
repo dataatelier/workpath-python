e1c185db2d4b247e52e82e97c50357d6aa210e35
Daniel Hafner <daniel-hafner1@gmx.de>
2020-05-16 15:12:46 +0200

    mvp closes #4

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="workpath",
+    name="workpathapi",
     version="0.0.1",
     author="Daniel Hafner",  # add yourself here setuptools
     author_email="daniel-hafner1@gmx.de",
-    description="Package to interact with workpath API",
+    description="Package to interact with workpathapi API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dataatelier/workpath-python",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires=">=3.8.3"  # more to come
+    python_requires=">=3.8.2"  # more to come
 )
---
+++ workpathapi/client.py
@@ -0,0 +1,79 @@
+from .helpers import create_params
+
+import requests
+import os
+
+
+class Client:
+    """
+    Class that handles the connection to the Workpath api.
+    """
+
+    # mandatory headers
+    _headers = {'Accept': 'application/json',
+                'Content-type': 'application/json'}
+    _base_url = 'https://{company}.workpath.com/api/v1'
+
+    def __init__(self, company: str, api_token: str = os.getenv('WORKPATH_API_TOKEN')):
+        self._base_url = self._base_url.format(company=company)
+        self._headers['Authorization'] = 'Token {api_token}'.format(api_token=api_token)
+
+    def get_goals(self, start_date=None, end_date=None, team_id=None, query=None, page=1, max_results=None) -> list:
+        """
+        Gets and returns all goals from the api.
+
+        :param start_date: start_date of the date range filter.
+                           Format: YYYY-MM-DD.
+                           When omitted all goals with target_date in the future are returned.
+                           Note: Required when end_date given.
+        :param end_date: end_date of the date range filter.
+                         Format: YYYY-MM-DD.
+                         When omitted all goals with target_date in the future are returned.
+                         Note: Required when start_date given.
+        :param team_id: Id of a Workpath team.
+                        When set only goals owned by that team will be returned.
+        :param query: Query to search against goal title, goal description, all key_result titles,
+                      and all key_result descriptions.
+        :param page: Number of first page.
+                     Default: first possible page
+        :param max_results: Limit of results. If None or larger than actual results all goals are returned.
+        :return: Returns all goals from the Workpath api in a list.
+        """
+        endpoint = 'goals'
+        url = '{base_url}/{endpoint}'.format(base_url=self._base_url, endpoint=endpoint)
+        params = create_params(start_date=start_date, end_date=end_date, team_id=team_id, query=query, page=page)
+        goals = []
+        total_pages = 1
+        # as long as there are goals left and have fewer goals then wanted
+        while params.get('page') <= total_pages and (not max_results or len(goals) < max_results):
+            # get more goals
+            response = self.request_data(url=url, params=params)
+            goals = goals + response.get(endpoint)
+            total_pages = response.get('pagination').get('total_pages')
+            params['page'] = params.get('page') + 1
+        # return all goals or max_results goals
+        return goals[:max_results]
+
+    def get_goal(self, goal_id: int) -> dict:
+        """
+        Gets and returns the data of a specific goal.
+
+        :param goal_id: Id of the goal.
+        :return: Data of the goal in a dict.
+        """
+        endpoint = 'goals'
+        url = '{base_url}/{endpoint}/{goal_id}'.format(base_url=self._base_url, endpoint=endpoint, goal_id=goal_id)
+        response = self.request_data(url=url)
+        return response
+
+    def request_data(self, url, params=None) -> dict:
+        """
+        Requests the data from the Workpath api.
+
+        :param url: Request url.
+        :param params: Optional parameters for get request.
+        :return: Json result of the request.
+        """
+        r = requests.get(url, headers=self._headers, params=params)
+        r.raise_for_status()
+        return r.json()
