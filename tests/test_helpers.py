006995f7a69121f2153de0f6b29c40e0a8ace819
Daniel Hafner <daniel-hafner1@gmx.de>
2020-05-28 17:51:53 +0200

    possibility to return pandas dataframe, closes #7

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,6 @@
 import unittest
 
-from workpathapi.helpers import create_params
+from workpathapi.helpers import create_params, list_to_dataframe
 
 
 class TestCreateParams(unittest.TestCase):
@@ -26,3 +26,45 @@ class TestCreateParams(unittest.TestCase):
     def test_create_params_missing_end_date(self):
         # function should raise a ValueError if only end date is provided
         self.assertRaises(ValueError, create_params, end_date='2020-01-02')
+
+
+class TestDictToDataFrame(unittest.TestCase):
+    import pandas as pd
+
+    simple_goals_dict = [{'description': 'Description', 'id': 1},
+                         {'description': 'Description', 'id': 2}]
+    simple_goals_df = pd.DataFrame(data={'description': ['Description', 'Description'], 'id': [1, 2]})
+
+    def test_dict_list_is_returned(self):
+        # output_format='dict'
+        self.assertEqual(self.simple_goals_dict,
+                         list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='dict'),
+                         "simple_goals_dict should return list of dicts if dict is passed as output format")
+
+        # output_format='json'
+        self.assertEqual(self.simple_goals_dict,
+                         list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='json'),
+                         "simple_goals_dict should return list of dicts if json is passed as output format")
+
+        # output_format='list'
+        self.assertEqual(self.simple_goals_dict,
+                         list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='list'),
+                         "simple_goals_dict should return list of dicts if list is passed as output format")
+
+    def test_dataframe_is_returned(self):
+        import pandas as pd
+
+        # output_format='DataFrame'
+        pd.testing.assert_frame_equal(self.simple_goals_df,
+                                      list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='DataFrame'))
+
+        # output_format='pd.DataFrame'
+        pd.testing.assert_frame_equal(self.simple_goals_df,
+                                      list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='DataFrame'))
+
+        # output_format='pandas.DataFrame'
+        pd.testing.assert_frame_equal(self.simple_goals_df,
+                                      list_to_dataframe(list_to_transform=self.simple_goals_dict, output_format='pandas.DataFrame'))
+
+    def test_value_error(self):
+        self.assertRaises(ValueError, list_to_dataframe, list_to_transform=self.simple_goals_dict, output_format='wrong')
--- workpathapi/helpers.py
+++ workpathapi/helpers.py
@@ -6,3 +6,23 @@ def create_params(**kwargs) -> dict:
                 (kwargs.get('start_date') and not kwargs.get('end_date')):
             raise ValueError('If start or end date is passed both start and end date need to be specified.')
     return {k: v for k, v in kwargs.items() if v is not None}
+
+
+def list_to_dataframe(list_to_transform, output_format):
+    """
+    Transform a list of dicts into a pandas DataFrame if wanted.
+
+    :param list_to_transform: the list with dicts to transform
+    :param output_format: Return a dictionary or pandas.DataFrame.
+                          Possible values: 'dict' or 'json' -> dictionary
+                                           'DataFrame', 'pd.DataFrame', or 'pandas.DataFrame' -> pandas.DataFrame
+
+    :return: List of dicts or pandas DataFrame with content of given dict.
+    """
+    if output_format in ['dict', 'json', 'list']:
+        return list_to_transform
+    elif output_format in ['DataFrame', 'pd.DataFrame', 'pandas.DataFrame']:
+        import pandas as pd
+        return pd.DataFrame.from_dict(list_to_transform)
+    else:
+        raise ValueError('Output format {format} is not supported'.format(format=output_format))
